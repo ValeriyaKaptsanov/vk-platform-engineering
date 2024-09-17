98dc2a6f57c45920d6c4e713dea5cf0b717d0fc2
root <root@DESKTOP-NKDHHFA>
2024-09-17 19:26:41 +0300

    Trying to integrate user inputs

--- user-input-tests/test.py
+++ user-input-tests/test.py
@@ -1,11 +1,37 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+# import os
 import platformtool
 
 app = Flask(__name__)
-@app.route("/")
+app.secret_key = 'aa'
+bootstrap = Bootstrap5(app)
+csrf = CSRFProtect(app)
+
+class ResourceForm(FlaskForm):
+    name = StringField('What resource do you want to use?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    
+@app.route("/", methods=['GET', 'POST'])
 def display_message():
-    return platformtool.main()
+    resources = 'ec2'
+    form = ResourceForm()
+    message = ""
+    if form.validate_on_submit():
+        name = form.name.data
+        if name.lower() == resources:
+            form.name.data = ""
+            message = "okay"
+        else:
+            message = "Fail"
+    return render_template('index.html', resources=resources, form=form, message=message)
+
+    
+    
+    # return platformtool.main()
     # os.system('#!/bin/sh python3 ../platformtool.py')
     # return "<p> Testing flask </p>"
 
